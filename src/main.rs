c4f38caf8cf0caf4ec9e2f83fb24c7726565bb6d
Paul Woolcock <paul@woolcock.us>
2015-04-24 11:25:48 -0400

    Update to rust-beta2

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,48 @@
-use std::os;
-use std::io::process::{Command,InheritFd,ExitStatus,ExitSignal};
+use std::env;
+// use std::io::process::{Command,InheritFd,ExitStatus,ExitSignal};
+use std::process::{exit, Command, Stdio, ExitStatus};
 
 use self::State::{Quoted, Escaped, Normal};
 
-enum State<'a> {
-    Quoted(&'a str),
+enum State {
+    Quoted(char),
     Escaped,
     Normal
 }
 
 fn trimmed(s: &str) -> String {
-    from_str(s.trim()).unwrap()
+    s.trim().to_string()
 }
 
-fn extract_commands<'a>(input: &'a str) -> Vec<String> {
+fn extract_commands(input: &str) -> Vec<String> {
     let mut state: State = Normal;
     let mut commands: Vec<String> = vec![];
-    let mut it = input.graphemes(true);
+    let mut it = input.chars();
     let mut s = String::new();
     loop {
         let c = match it.next() {
             Some(ch) => ch.clone(),
             None => {
-                commands.push(trimmed(s.as_slice()));
+                commands.push(trimmed(&s));
                 return commands;
             }
         };
         match state {
             Normal => {
                 match c {
-                    "\\" => {
+                    '\\' => {
                         state = Escaped;
                     },
-                    "'" | "\"" => {
+                    '\'' | '"' => {
                         state = Quoted(c);
-                        s.push_str(c);
+                        s.push(c);
                     },
-                    "," => {
-                        commands.push(trimmed(s.as_slice()));
+                    ',' => {
+                        commands.push(trimmed(&s));
                         s.clear();
                     },
                     _ => {
-                        s.push_str(c);
+                        s.push(c);
                     },
                 }
             },
@@ -49,11 +50,11 @@ fn extract_commands<'a>(input: &'a str) -> Vec<String> {
                 if c == terminator {
                     state = Normal;
                 }
-                s.push_str(c);
+                s.push(c);
             },
             Escaped => {
                 state = Normal;
-                s.push_str(c);
+                s.push(c);
             },
         }
     }
@@ -68,32 +69,31 @@ fn main() {
     // But, we keep the `cargo`, because we don't want
     // to just assume that `cargo` is the name of the cargo
     // binary, nor do we want to assume that it is on our path
-    match os::args().as_slice() {
-        [_, ref binname, _, rest..] => {
-            let args = rest.connect(" ");
-            for command in extract_commands(args.as_slice()).iter() {
-                let status = Command::new(binname)
-                                .args(&[command])
-                                .stdin(InheritFd(0))
-                                .stdout(InheritFd(1))
-                                .stderr(InheritFd(2))
-                                .status();
-                match status {
-                    Ok(ExitStatus(0)) => (),
-                    Ok(ExitStatus(i)) => {
-                        os::set_exit_status(i)
-                    },
-                    Ok(ExitSignal(i)) => {
-                        os::set_exit_status(i)
-                    },
-                    Err(_) => {
-                        os::set_exit_status(127)
-                    },
+    let mut args = env::args();
+    let binname = args.nth(1).unwrap();
+    let args = args.skip(1).collect::<Vec<_>>().connect(" ");
+
+    for command in extract_commands(&args).iter() {
+        let status = Command::new(&binname)
+                        .args(&[command])
+                        .stdin(Stdio::inherit())
+                        .stdout(Stdio::inherit())
+                        .stderr(Stdio::inherit())
+                        .status();
+        match status {
+            Ok(st) => {
+                if !st.success() {
+                    match st.code() {
+                        Some(i) => { exit(i); },
+                        None => { exit(127); }
+                    }
                 }
-            }
-        },
-        _ => {
-            os::set_exit_status(127);
+
+                exit(0);
+            },
+            Err(_) => {
+                exit(127);
+            },
         }
     }
 }
